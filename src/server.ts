922dfd30713cc3f38122f349d6054c6ea1554e92
Lelouching <engenheirogamer123@gmail.com>
2023-03-17 02:35:33 -0300

    feat: Entity Artist from table kpop_artists created

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ AppDataSource.initialize()
   .then(async () => {
     console.log("Database connected.")
 
-    const PORT = process.env.PORT || 3000
+    const PORT: number = Number(process.env.PORT) || 3000
     app.listen(PORT, () => {
       console.log(`Server is running on https://localhost:${PORT}`)
     })
