a8d3fc0ab4455db7b763c48750ba2c64809edb9c
Lelouching <engenheirogamer123@gmail.com>
2023-05-03 10:15:59 -0300

    change: changed kpopArtist schema, numbers are not decimal anymore, only integer

--- src/schemas/kpopArtists.schema.ts
+++ src/schemas/kpopArtists.schema.ts
@@ -1,23 +1,20 @@
 import { z } from "zod"
 
-const validateFloatNumber = (number: number) => parseFloat(number.toFixed(2))
-
 export const kpopArtistCreateSchema = z.object({
     name: z.string().max(20),
     description: z.string().max(350).nullish(),
     image: z.string().max(256),
-    vocal: z.number().positive().max(10).transform((number) => validateFloatNumber(number)),
-    dance: z.number().positive().max(10).transform((number) => validateFloatNumber(number)),
-    standardKorean: z.number().positive().max(10).transform((number) => validateFloatNumber(number)),
-    popularity: z.number().positive().max(10).transform((number) => validateFloatNumber(number)),
-    rap: z.number().positive().max(10).transform((number) => validateFloatNumber(number)),
-    stagePresence: z.number().positive().max(10).transform((number) => validateFloatNumber(number)),
+    vocal: z.number().positive().int().max(10),
+    dance: z.number().positive().int().max(10),
+    standardKorean: z.number().positive().int().max(10),
+    popularity: z.number().positive().int().max(10),
+    rap: z.number().positive().int().max(10),
+    stagePresence: z.number().positive().int().max(10),
     musicalGroup: z.string().max(50)
 })
 
 export const kpopArtistInfoSchema = kpopArtistCreateSchema.extend({
     id: z.number(),
     createdAt: z.date(),
-    updatedAt: z.date(),
-    avaragePoints: z.number().positive().max(10)
+    updatedAt: z.date()
 })
